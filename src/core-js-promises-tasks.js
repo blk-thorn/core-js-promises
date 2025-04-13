c3c4fa2db6d51094d1cb949d8e39e20b309a36d8
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2025-04-13 22:52:35 +0300

    feat: solve getFirstResolvedPromiseResult, getFirstPromiseResult, getAllOrNothing tasks

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -59,8 +59,21 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return new Promise((resolve, reject) => {
+    let rejectedCount = 0;
+    promises.forEach((promise) => {
+      promise.then(
+        (value) => resolve(value),
+        () => {
+          rejectedCount += 1;
+          if (rejectedCount === promises.length) {
+            reject();
+          }
+        }
+      );
+    });
+  });
 }
 
 /**
@@ -82,8 +95,8 @@ function getFirstResolvedPromiseResult(/* promises */) {
  * [promise3, promise6, promise2] => Promise rejected with 2
  * [promise3, promise4, promise6] => Promise rejected with 6
  */
-function getFirstPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstPromiseResult(promises) {
+  return Promise.race(promises);
 }
 
 /**
@@ -97,8 +110,8 @@ function getFirstPromiseResult(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  return Promise.all(promises);
 }
 
 /**
